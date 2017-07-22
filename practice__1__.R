1cbc63c04dcc8614fe03c5fcd1bb1fcaa2bfe755
johnypark <g2njoy@ufl.edu>
2017-07-22 09:57:54 -0400

    072217

--- practice__1__.R
+++ practice__1__.R
@@ -11,18 +11,26 @@ trait_model<-function(df){
 groupby_mtcars<-mtcars %>% group_by(cyl)%>%nest()
 
 trait_model<-function(df,x.var,y.var){
-  df %$%loess(.[[y.var]]~.[[x.var]])  
+  df %$%lm(.[[y.var]]~.[[x.var]])  
 }
 
 
 model_mtcars<-groupby_mtcars %>% 
   mutate(
     
-    model=data%>%map(trait_model,"wt","mpg")
-    
+    model=data%>%map(trait_model,"wt","mpg"),
+    glance=model%>%map(broom::glance),
+    tidy   = map(model, broom::tidy)
     
     
   )
+
+model_mtcars%>%unnest(glance)
+model_mtcars%>%unnest(tidy)
+model_mtcars%>%unnest(model) ##does not work
+
+
+
 
 model_mtcars<-mtcars %>% group_by(cat)%>% nest() %>%
   mutate(
--- practice__2__.R
+++ practice__2__.R
@@ -6,4 +6,14 @@ library(tidyverse)
 
 mtcars%<>%data.table
 mtcars%>%head
-mtcars[,glance(lm(mpg~wt)),by=cyl]
+mtcars[,glance(lm(mpg~wt)),by=cyl] ##same as model_mtcars %>%unnest(glance)
+mtcars[,tidy(lm(mpg~wt)),by=cyl]
+mtcars[,tidy(lm("mpg"~"wt")),by=cyl] ##however cannot pass dynamic varibales 1
+mtcars[,tidy(lm([["mpg"]]~[["wt"]])),by=cyl] ##however cannot pass dynamic varibales 2
+mtcars[,mean(mpg),by=cyl] ##still lots of potential in data table 
+
+
+dy_lm<-function(data,xvar,yvar,...) data%$%lm(.[[yvar]]~.[[xvar]],...)
+
+mtcars[,augment(loess(mpg~wt)),by=cyl]
+mtcars[,augment(loess(mpg~wt))]
